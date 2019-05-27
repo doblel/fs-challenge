1c90cb1380990c36f2a3fd81188193e04d56d06a
Lisandro Lucatti <lucattilisandro@gmail.com>
2019-05-27 15:29:16 -0300

    [FIX] Update syntax

--- client/src/api.js
+++ client/src/api.js
@@ -2,21 +2,21 @@ const API_URL = 'http://localhost:5000/api'
 
 export default {
   getAccounts: () => fetch(`${API_URL}/accounts`),
-  createAccount: account => fetch(`${API_URL}/accounts`, {
+  createAccount: (account) => fetch(`${API_URL}/accounts`, {
     method: 'POST',
     body: JSON.stringify(account),
     headers: {
       'Content-Type': 'application/json'
     }
   }),
-  updateAccount: account => fetch(`${API_URL}/accounts/${account.id}`, {
+  updateAccount: (account) => fetch(`${API_URL}/accounts/${account.id}`, {
     method: 'PUT',
     body: JSON.stringify(account),
     headers: {
       'Content-Type': 'application/json'
     }
   }),
-  deleteAccount: accountId => fetch(`${API_URL}/accounts/${accountId}`, {
+  deleteAccount: (accountId) => fetch(`${API_URL}/accounts/${accountId}`, {
     method: 'DELETE',
   })
 }
